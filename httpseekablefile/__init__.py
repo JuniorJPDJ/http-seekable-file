de219e63200d70a45b8d25c0d8964e32795bd349
JuniorJPDJ <git@juniorjpdj.pl>
2020-08-18 22:10:22 +0200

    Folder rename back

--- httpseekablefile/__init__.py
+++ httpseekablefile/__init__.py
@@ -1,9 +1,12 @@
 __version__ = '0.1.0'
 
+
+__all__ = []
+
 try:
     import aiohttp
 
     from .AsyncSeekableHTTPFile import AsyncSeekableHTTPFile
+    __all__.append(AsyncSeekableHTTPFile)
 except ImportError:
     pass
-
